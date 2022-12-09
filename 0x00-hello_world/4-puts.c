d653ae999e7af46a5baa979f5475406945e16029
simpleflex <smartmichael17@gmail.com>
2022-12-08 23:49:35 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <studio.h>
+#include <stdio.sh>
 /**
  * main - Entry
- * Return: Always 0
-*/
+ * Return:Always 0
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle puzzle");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle")\
+		return (0);
 }
