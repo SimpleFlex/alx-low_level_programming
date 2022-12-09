3e96fbe812b60e10422b1eccfa738f2383010e4a
simpleflex <smartmichael17@gmail.com>
2022-12-09 00:14:32 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/** 
- *  * main - Entry
- *  * Return: Always 0
+/**
+ * main - Entry
+ * Return: Always 0
  */
 main(void)
 	int main(void)
@@ -10,8 +10,8 @@ main(void)
 	printf("Size of an int: %i byte(s)\n", sizeof(int));
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %i byte(s)\n", sizof(long long int));
-	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %i byte(s)\n", sizof(float));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %i byte(s)\n", sizeof(float));
-		return (0);
+	return (0);
 }
