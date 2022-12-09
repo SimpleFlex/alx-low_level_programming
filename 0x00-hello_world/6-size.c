6fd76bcce1eb4fc9a26433eec40cc933b289bac5
simpleflex <smartmichael17@gmail.com>
2022-12-09 02:00:12 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 
 	printf("Size of a char: %lu byte(S)\n", (unsigned long)sizeof(a));
 	printf("size of an int: %lu byte(S)\n", (unsigned long)sizeof(b));
-	printf("size of a long int: %x%x\n", *(((int *)(&longint)) + 1), longint);
+	printf("size of a long int: %lu byte(S)\n", (unsigned long)sizeof(c));
 	printf("size of a long long int: %lu byte(S)\n", (unsigned long)sizeof(d));
 	printf("size of a float: %lu bytes(S)\n", (unsigned long)sizeof(f));
 	return (0);
