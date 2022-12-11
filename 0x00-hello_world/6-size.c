06cf23e48c51296358e15c574c8c827463d392c8
simpleflex <smartmichael17@gmail.com>
2022-12-11 13:06:48 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,13 +3,13 @@
  * main - done bysimpleflex
  * Return: Always 0
  */
-main(void)
+main()
 int main(void)
 {
 	printf("size of char: %i byte(s)\n", sizeof(char));
 	printf("size of int: %i byte(s)\n", sizeof(int));
 	printf("size of long int: %i byte(s)\n", sizeof(long int));
 	printf("size of long long int: %i byte(s)\n", sizeof(long long int));
-	printf("size of float: %i bytes(s)\n", sizeof(float));
+	printf("size of float: %i byte(s)\n", sizeof(float));
 	return (0);
 }
