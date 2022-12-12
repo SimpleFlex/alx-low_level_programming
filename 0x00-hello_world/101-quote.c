2a809e4b832d58144be7d1b4cf0ae81e35008930
simpleflex <smartmichael17@gmail.com>
2022-12-12 13:33:21 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,8 @@
  * main - Entry
  * Return: Always 1
  */
-main()
 int main(void)
-{77
-	write("and that piece of art is useful" - Dora Korpar, 2015- 10- 19\n",59);
+{
+	write(1, "and that piece of art is useful\" -Dora Korpar 2015- 10- 19\n", 59);
 	return (1);
 }
