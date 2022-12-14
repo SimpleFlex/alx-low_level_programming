d6b4dfcd8026d1dde30443b9c46b15bd6a0d635e
simpleflex <smartmichael17@gmail.com>
2022-12-14 04:02:28 -0800

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for alphabets
+ * _isalpha - checks if the character is part of the alphabet
  * @c: character to be checked
  *
- * Retun: 1 if character is a letter, 0 otherwise
+ * Return: 1 if @c is a letter,otherwise 0
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'z'))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 		return (1);
 	else
 		return (0);
-
+}
