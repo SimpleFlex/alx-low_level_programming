48b75de9ef4c07a4b162e460b3d98e1afa246402
simpleflex <smartmichael17@gmail.com>
2022-12-14 07:14:14 -0800

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbersfrom n to 98
+ *
+ * @n: start of the natural number
+ *
+ * Return: nothing on success
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+
+			if (n != 98)
+				printf(",");
+			n++;
+		}
+	}
+	putchar('\n');
+}
