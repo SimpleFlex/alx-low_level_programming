39bda1bd8d130acf197339b4086c32b009531cf2
simpleflex <smartmichael17@gmail.com>
2022-12-14 06:17:42 -0800

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute 
+ * jack_bauer - prints every minute
  * of the day of jackbauer, \ starting from 00:00 to 23:59
  *
  * Return: nothing on success
