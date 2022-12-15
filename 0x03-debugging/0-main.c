82d5a334aed9c7b33ac719ed05a3ed529341b865
simpleflex <smartmichael17@gmail.com>
2022-12-15 01:22:34 -0800

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,16 @@
 #include "main.h"
+
 /**
- * main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+ * main - test function thta prints if integers is positibe or negative
+ * Return: 0
+ */
+
 int main(void)
 {
-        int i;
-        i = 98;
+	int i;
 
-        positive_or_negative(i);
+	i = 98;
+	positive_or_negative(i);
 
-        return (0);
+	return (0);
 }
