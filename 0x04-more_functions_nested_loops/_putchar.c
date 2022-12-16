a8e143aa98d6e781ee80097391e4391c8f2b23bb
simpleflex <smartmichael17@gmail.com>
2022-12-16 02:01:23 -0800

    task 4

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * _putchar - writes the character c to standout
+ * @char: the character to print
+ * Return: 1 on success
+ * on error, -1 is returned, and no err is set to appropraite
+ */
+int _putchar(char)
+{
+	return (write(1, &c, 1));
+}
