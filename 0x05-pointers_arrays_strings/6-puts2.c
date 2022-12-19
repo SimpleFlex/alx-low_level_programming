947964e7ef904ddd32f5aedcce0259ae608beb15
simpleflex <smartmichael17@gmail.com>
2022-12-19 04:47:36 -0800

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * puts2 - print the first character
+ * @str: input 
+ * Return: print it
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue;
+	}
+	_putchar('\n');
+}
