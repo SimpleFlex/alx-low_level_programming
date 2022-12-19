80a7a644999b6b1a943355834162561a3f437bcf
simpleflex <smartmichael17@gmail.com>
2022-12-19 13:48:05 -0800

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -5,18 +5,19 @@
  * print_array - print array elements
  * @a: array
  * @n: number of value to be printed
- * :Return: no return
+ * Return: return a and n inputs
  */
 void print_array(int *a, int n)
 {
-	int t = 0;
+	int p;
 
-	for (; t < n; t++)
+	for (p = 0; p < (n - 1); p++)
 	{
-		printf("%d", *(a + t));
-		if (t != n - 1)
-			printf(",");
+		printf("%d, ", a[p]);
 	}
-
+		if (p == (n - 1))
+		{
+			printf("%d", a[n - 1]);
+		}
 	printf("\n");
 }
