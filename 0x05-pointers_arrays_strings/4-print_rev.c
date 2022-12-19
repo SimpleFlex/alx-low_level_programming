7a78e14cba4a8b96461f50135ad9d864e8b1f6c8
simpleflex <smartmichael17@gmail.com>
2022-12-19 04:28:05 -0800

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string reverse
+ * @s: string printed
+ */
+void print_rev(char *s)
+{
+	int = 0;
+
+	while (s[i] != '\0')
+		i++;
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
