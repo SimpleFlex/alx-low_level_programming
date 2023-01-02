b38a96e5a5c476a6cd9446ccde8eee9e2d1efbf8
simpleflex <smartmichael17@gmail.com>
2023-01-02 09:15:47 -0800

    task 1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,3 @@
-/*
- * File: 0-memset.c
- * Auth: not
- */
-
 #include "main.h"
 
 /**
@@ -15,10 +10,10 @@
  *
  * Return: A pointer to the filled memory area @s.
  */
-void *_memset(void *s, int c, size_t n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int index;
-	unsigned char *memory = s, value = c;
+	unsigned char *memory = b, value = c;
 
 	for (index = 0; index < n; index++)
 		memory[index] = value;
