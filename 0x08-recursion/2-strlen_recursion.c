3f90e0e25debc8c783e9f0dad00acfa30ac3f891
simpleflex <smartmichael17@gmail.com>
2023-01-05 11:57:37 -0800

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the long line
+ * @s: the string to be measured
+ * Return: longness of the strings
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len+=;
+		_strlen_recursion(s + 1);
+	}
+
+	return (len);
+}
