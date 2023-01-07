72f82f1f334759c4b3254b9bef55154659d67d93
simpleflex <smartmichael17@gmail.com>
2023-01-06 17:15:07 -0800

    task 0

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,29 +1,20 @@
 #include "main.h"
  /**
  * _strcat - concatenates two strings
- * @dest: input value
- * @src: input value
+ * @dest: a pointerto the string to be concated upon
+ * @src: the source string to be appended to @dest.
  *
- * Return: void
+ * Return: A pointer to the destination string @dest
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int index = 0, dest_len = 0;
 
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
+	while (dest[index++])
+		dest_len++;
 
-	dest[i] = '\0';
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
 }
