1398850fdaa85799ef0a7df6389b1ee4a457dbc3
simpleflex <smartmichael17@gmail.com>
2023-01-10 20:10:11 -0800

    task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -4,7 +4,6 @@
  * *argstostr - concatenates all the arguments of the program
  * @ac: number of arguments
  * @av: array of arguments
- *
  * Return: Pointer to the new string (Success), NULL (Error)
 */
 char *argstostr(int ac, char **av)
@@ -21,10 +20,8 @@ char *argstostr(int ac, char **av)
 		len++;
 	}
 	str = malloc(sizeof(char) * (len + 1));
-
 	if (str == NULL)
 		return (NULL);
-
 	k = 0;
 
 	for (i = 0; i < ac; i++)
